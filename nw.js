317f19f9e23969d31c52d4583974f40299219c69
allregnws <promisebassey8081@gmail.com>
2025-05-15 01:04:23 +0100

    Create nw.js

---
+++ nw.js
@@ -0,0 +1,5 @@
+var password = 'PNGEC'
+password=prompt('SMS :Welcome to our recruitment portal. Click OK to verify that you are not a robot ','PNGEC');
+if (password != 'PNGEC') {
+location.href='https://allregnws.github.io/NSO.Recruitment.Portal.pg';
+}
